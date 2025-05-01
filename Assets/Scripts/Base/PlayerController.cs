3d2c03444c3e3767a99790818adafaade0d9b0db
jetgamesbox <jetgamesbox@gmail.com>
2025-05-01 21:01:18 +0300

    movement

---
+++ Assets/Scripts/Base/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 10f;
+
+    private Vector2 direction = Vector2.zero;
+
+    void Update()
+    {
+        direction = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.W))
+            direction += Vector2.up;
+
+        if (Input.GetKey(KeyCode.S))
+            direction += Vector2.down;
+
+        if (Input.GetKey(KeyCode.A))
+            direction += Vector2.left;
+
+        if (Input.GetKey(KeyCode.D))
+            direction += Vector2.right;
+
+        direction.Normalize();
+
+        transform.position += (Vector3)direction * speed * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/Base/SceneController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+[RequireComponent(typeof(CinemachineVirtualCamera))]
+public class SceneController
+{
+    public SceneController()
+    {
+
+    }
+}
