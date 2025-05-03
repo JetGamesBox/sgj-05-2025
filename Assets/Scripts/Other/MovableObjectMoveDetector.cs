1b93abaccf14509db08cec12c2d077f5695ba82d
Corbie2555 <corbievolume@gmail.com>
2025-05-03 17:04:07 +0300

    MovingCupSound fix

--- Assets/Scripts/Other/MovableObjectMoveDetector.cs
+++ Assets/Scripts/Other/MovableObjectMoveDetector.cs
@@ -2,20 +2,77 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class MovableObjectMoveDetector : MonoBehaviour
 {
-    [SerializeField] private Transform target;
+    [Header("Movement Detection")]
+    [SerializeField] private Transform target;  // The player/object that can move this
 
-    public bool Moving = false;
+    [Header("FMOD Sound")]
+    [SerializeField] private FMODUnity.EventReference movingSoundEvent;
+
+    public bool Moving { get; private set; }
+    private FMOD.Studio.EventInstance movingSoundInstance;
+
+    private void Awake()
+    {
+        movingSoundInstance = FMODUnity.RuntimeManager.CreateInstance(movingSoundEvent);
+        Moving = false;
+    }
+
+    private void Update()
+    {
+        if (Moving)
+        {
+            PlaySound();
+        }
+        else
+        {
+            StopSound();
+        }
+    }
+
+    private void PlaySound()
+    {
+        FMOD.Studio.PLAYBACK_STATE playbackState;
+        movingSoundInstance.getPlaybackState(out playbackState);
+
+        if (playbackState != FMOD.Studio.PLAYBACK_STATE.PLAYING)
+        {
+            movingSoundInstance.start();
+        }
+    }
+
+    private void StopSound()
+    {
+        FMOD.Studio.PLAYBACK_STATE playbackState;
+        movingSoundInstance.getPlaybackState(out playbackState);
+
+        if (playbackState == FMOD.Studio.PLAYBACK_STATE.PLAYING)
+        {
+            movingSoundInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        }
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (!Moving && collision.transform == target)
+        {
             Moving = true;
+        }
     }
+
     private void OnCollisionExit2D(Collision2D collision)
     {
         if (Moving && collision.transform == target)
+        {
             Moving = false;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        movingSoundInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+        movingSoundInstance.release();
     }
 }
